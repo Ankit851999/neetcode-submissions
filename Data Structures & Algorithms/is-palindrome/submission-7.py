23e962e71029b239ddf34187c06f8f6918543b3c
Ankit851999 <ankitkumar851999@gmail.com>
2026-07-03 12:54:54 +0530

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = [i.lower() for i in s if i.isalpha() or i.isdigit()]
+        for i in range(len(string) // 2):
+            if string[i] != string[-(i + 1)]:
+                return False
+
+        return True
